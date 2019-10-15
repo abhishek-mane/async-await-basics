3c3345cdf1b8467073542ad0f9d96cd1f4b98403
Abhishek Mane <abhishekmane@outlook.in>
2019-10-15 14:23:33 -0400

    promis.all

---
+++ 3.promis_all.js
@@ -0,0 +1,35 @@
+// author : Abhishek Mane
+
+/*
+    People in a queue for movie using async/await
+*/
+
+const getTicket = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('ticket')
+    }, 2000)
+});
+
+console.log("Person 1 in")
+console.log("Person 2 in")
+
+// =============================================================================
+doTasks = async () => {
+
+    const getPopcorn = new Promise((resolve, reject) => resolve('popcorn'));
+    const getCoke = new Promise((resolve, reject) => resolve('coke'));
+
+    let [t, p, c] = await Promise.all([getTicket, getPopcorn, getCoke])
+    console.log(`got ${t} ${p} ${c}`)
+
+    return 'all done'
+};
+
+doTasks().then((msg) => {
+    console.log(msg)
+    console.log('Person 3 in')
+})
+// =============================================================================
+
+console.log("Person 4 in")
+console.log("Person 5 in")
